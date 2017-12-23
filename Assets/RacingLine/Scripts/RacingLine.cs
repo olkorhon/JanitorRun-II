4afb021970106dc12ee13f0f7bd68970ffe301a7
Lari-Matias Orjala <lorjala@student.oulu.fi>
2017-12-23 14:19:54 +0200

    Finished the line, added visual helpers for the scene view.

--- Assets/RacingLine/Scripts/RacingLine.cs
+++ Assets/RacingLine/Scripts/RacingLine.cs
@@ -6,51 +6,56 @@ using UnityEditor;
 [RequireComponent(typeof(LineRenderer))]
 public class RacingLine : MonoBehaviour
 {
-    public RacingLineConfig config;
-    public LineRenderer lineRenderer;
-    public Material material;
+    public RacingLineConfig config; // ScriptableObject asset containing the control points.
+    public LineRenderer lineRenderer; // Line renderer component reference.
+    public Material material; // Material for the line.
+    public int pointCount = 10; // Number of curve points per segment to be calculated.
 
-    private int pointCount = 0;
-    private int numCurves = 0;
-    private List<Vector3> pathPoints = new List<Vector3>();
+    // Display helpers:
+    public bool displayGuides = true; // Display handles in editor.
+    public bool displayLabels = true; // Display control point index labels with handles.
+
+    private int numCurves = 0; // Number of segments.
+    private List<Vector3> pathPoints = new List<Vector3>(); // Calculated path points.
 
     private void Start()
     {
+        // Get line renderer reference.
         if (!lineRenderer)
             lineRenderer = (LineRenderer)GetComponent<LineRenderer>();
 
+        // Set the line material.
         lineRenderer.material = material;
     }
 
     private void Update()
     {
-        /*
-        UpdatePath();
+        // Calculate the number of segments for path calculation.
+        InitPath();
 
-        for (int i = 1; i < pointCount; i++)
-        {
-            Vector3 start = pathPoints[i - 1];
-            Vector3 end = pathPoints[i];
-            DrawLine(start, end, .5f, Color.white);
-        }
-        */
-
-        numCurves = config.controlPoints.Count / 3;
-
-        if (numCurves > 0 && config.controlPoints.Count % 3 == 0)
-            numCurves--;
-        
-        pointCount = 10;
-
+        // Calculate the points.
         CalculatePath();
 
+        // Draw the line segments.
         DrawCurve();
+    }
+
+    void InitPath()
+    {
+        if (config.controlPoints.Count == 0) return;
+
+        // Get segment count.
+        numCurves = (int)((config.controlPoints.Count - 1) / 3);
     }
 
     void CalculatePath()
     {
+        if (pointCount <= 0) return;
+
+        // Clear old path.
         pathPoints.Clear();
 
+        // Calculate each segment:
         for (int c = 0; c < numCurves; c++)
         {
             int nodeIndex = c * 3;
@@ -60,6 +65,7 @@ public class RacingLine : MonoBehaviour
             Vector3 p2 = config.controlPoints[nodeIndex + 2];
             Vector3 p3 = config.controlPoints[nodeIndex + 3];
 
+            // Calculate each point in the segment:
             for (int p = 0; p <= pointCount; p++)
             {
                 float t = p / (float)pointCount;
@@ -74,62 +80,15 @@ public class RacingLine : MonoBehaviour
     {
         for (int i = 0; i < pathPoints.Count; i++)
         {
-            //Vector3 start = pathPoints[i - 1];
             Vector3 point = pathPoints[i];
             lineRenderer.positionCount = pathPoints.Count;
             lineRenderer.SetPosition(i, point);
-            //DrawLine(start, end, .5f, Color.white);
-        }
-
-        /*
-        for (int c = 0; c < numCurves; c++)
-        {
-            int nodeIndex = c * 3;
-
-            Vector3 p0 = config.controlPoints[nodeIndex];
-            Vector3 p1 = config.controlPoints[nodeIndex + 1];
-            Vector3 p2 = config.controlPoints[nodeIndex + 2];
-            Vector3 p3 = config.controlPoints[nodeIndex + 3];
-
-            for (int p = 1; p <= pointCount; p++)
-            {
-                float t = p / (float)pointCount;
-
-                Vector3 point = CalculateBezier(p0, p1, p2, p3, t);
-                lineRenderer.positionCount = (c * pointCount) + p;
-                lineRenderer.SetPosition((c * pointCount) + (p - 1), point);
-            }
-        }
-        */
-    }
-
-    /*
-    void CreateCurve()
-    {
-        segments = config.controlPoints.Count / 3;
-
-        for (int s = 0; s < config.controlPoints.Count - 3; s += 3)
-        {
-            Vector3 p0 = config.controlPoints[s];
-            Vector3 p1 = config.controlPoints[s + 1];
-            Vector3 p2 = config.controlPoints[s + 2];
-            Vector3 p3 = config.controlPoints[s + 3];
-
-            if (s == 0)
-            {
-                pathPoints.Add(CalculateBezier(p0, p1, p2, p3, 0.0f));
-            }
-
-            for (int p = 0; p < (pointCount / segments); p++)
-            {
-                float t = (1.0f / (pointCount / segments)) * p;
-                Vector3 point = CalculateBezier(p0, p1, p2, p3, t);
-                pathPoints.Add(point);
-            }
         }
     }
-    */
 
+    /**
+     * Calculates a point in cubic bezier curve at t.
+     */
     Vector3 CalculateBezier(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
     {
         float tt = t * t;
@@ -146,43 +105,29 @@ public class RacingLine : MonoBehaviour
         return p;
     }
 
-    void DrawLine(Vector3 start, Vector3 end, float lineSize, Color color)
-    {
-        //lineRenderer.material = material;
-        //lineRenderer.material.color = color;
-        //lineRenderer.startWidth = lineSize;
-        //lineRenderer.endWidth = lineSize;
-        //lineRenderer.positionCount = 2;
-        //lineRenderer.SetPosition(0, start);
-        //lineRenderer.SetPosition(1, end);
-    }
-
     void OnDrawGizmos()
     {
+        if (!displayGuides) return;
+
         for (int cp = 0; cp < config.controlPoints.Count; cp++)
         {
             Handles.color = Color.green;
             Handles.DrawSolidDisc(config.controlPoints[cp], Vector3.up, .08f);
+            if (displayLabels)
+                Handles.Label(config.controlPoints[cp], cp.ToString());
         }
+
+        InitPath();
 
         CalculatePath();
-        for (int i = 0; i < pathPoints.Count; i++)
-        {
-            Vector3 point = pathPoints[i];
 
-            Gizmos.color = Color.cyan;
-            Gizmos.DrawSphere(point, .1f);
-        }
-
-        /*
-        UpdatePath();
-        for (int i = 1; i < (path.pointCount); i++)
+        for (int i = 1; i < pathPoints.Count; i++)
         {
-            Vector3 startv = path.pathPoints[i - 1];
-            Vector3 endv = path.pathPoints[i];
-            Gizmos.color = Color.blue;
-            Gizmos.DrawLine(startv, endv);
+            Vector3 first = pathPoints[i - 1];
+            Vector3 last = pathPoints[i];
+
+            Handles.color = Color.yellow;
+            Handles.DrawLine(first, last);
         }
-        */
     }
 }
