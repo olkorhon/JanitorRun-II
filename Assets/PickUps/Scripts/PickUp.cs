3b35a276af7b2caa94f4f0d7c586f6d3f12879ed
Olli Korhonen <olkorhon@gmail.com>
2018-02-02 18:29:47 +0200

    added component for player boosting, boost in testable state

--- Assets/PickUps/Scripts/PickUp.cs
+++ Assets/PickUps/Scripts/PickUp.cs
@@ -12,6 +12,7 @@ using UnityEngine;
     @Author: Jussi Sepponen, Olli Korhonen
 */
 
+[RequireComponent(typeof(Collider))]
 public class PickUp : MonoBehaviour
 {
     [Space(10)]
@@ -25,39 +26,71 @@ public class PickUp : MonoBehaviour
     public float currentOscillation = 0.0f;
     private float oscillationDirection = 1.0f;
     
-
     private Vector3 originalPosition;
+    private Collider mainCollider;
+
+    private Vector3 startingPosition;
+    private Transform target;
+    private float consumingLeft = 0.0f;
+    private float consumeSpeed = 1.0f;
+
+    private bool consuming = false;
 
     void Start()
     {
         this.originalPosition = transform.position;
+        this.mainCollider = GetComponent<Collider>();
     }
 
     // Update is called once per frame
     void Update ()
     {
-        transform.localRotation = Quaternion.Euler(transform.localRotation.eulerAngles + new Vector3(0, this.rotationSpeed * Time.deltaTime, 0));
+        if (!this.consuming)
+        {
+            transform.localRotation = Quaternion.Euler(transform.localRotation.eulerAngles + new Vector3(0, this.rotationSpeed * Time.deltaTime, 0));
 
-        this.currentOscillation += oscillationDirection * oscillationSpeed * Time.deltaTime;
-        this.oscillationDirection *= Mathf.Abs(currentOscillation) >= 1.0f ? -1 : 1; // Reverse oscillation if over limits
-        this.currentOscillation = Mathf.Clamp(this.currentOscillation, -1.0f, 1.0f); // Clamp oscillation to -1.0f and 1.0f
+            this.currentOscillation += oscillationDirection * oscillationSpeed * Time.deltaTime;
+            this.oscillationDirection *= Mathf.Abs(currentOscillation) >= 1.0f ? -1 : 1; // Reverse oscillation if over limits
+            this.currentOscillation = Mathf.Clamp(this.currentOscillation, -1.0f, 1.0f); // Clamp oscillation to -1.0f and 1.0f
 
-        float value = Mathf.Pow(Mathf.Abs(this.currentOscillation), 1.0f / 1.2f) * Mathf.Sign(this.currentOscillation) * this.oscillationMax;
+            float value = Mathf.Pow(Mathf.Abs(this.currentOscillation), 1.0f / 1.2f) * Mathf.Sign(this.currentOscillation) * this.oscillationMax;
 
-        transform.position = this.originalPosition + new Vector3(0, value, 0);
-    }
-
-    float CubeRoot(float d)
-    {
-        return Mathf.Pow(Mathf.Abs(d), 1f / 3f) * Mathf.Sign(d);
+            transform.position = this.originalPosition + new Vector3(0, value, 0);
+        }
+        else if (this.consumingLeft > 0)
+        {
+            transform.localScale = new Vector3(this.consumingLeft, this.consumingLeft, this.consumingLeft);
+            transform.position = Vector3.Lerp(target.position, startingPosition, this.consumingLeft);
+            this.consumingLeft -= Time.deltaTime * consumeSpeed;
+        }
+        else
+        {
+            this.consumeSpeed = 0.0f;
+            this.consuming = false;
+            Destroy(this.gameObject);
+        }
     }
 
     public void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            //TODO something
-            Destroy(this.gameObject);
+            Debug.Log("PickUp collided with player");
+            Consumer consumer = other.gameObject.GetComponent<Consumer>();
+
+            if (consumer == null)
+            {
+                Destroy(this.gameObject);
+                return;
+            }
+
+            mainCollider.enabled = false;
+            this.startingPosition = transform.position;
+            this.target = consumer.getMouth();
+
+            this.consumeSpeed = consumer.getConsumeSpeed();
+            this.consumingLeft = 1.0f;
+            this.consuming = true;
         }
     }
 }
---
+++ Assets/Player/Scripts/Booster.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Booster : MonoBehaviour
+{
+    [Space(10)]
+    private Animator animator;
+
+    public float boostMultiplier = 2.0f;
+    public float maxBoostDuration = 5.0f;
+    public float windupDuration = 1.0f;
+
+    [Space(6)]
+    bool isBoosting = false;
+    public float boostLeft = 0.0f;
+
+    // Use this for initialization
+    void Start ()
+    {
+        this.animator = GetComponent<Animator>();
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            activateBoost();
+        }
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (!this.isBoosting)
+            return;
+
+        if (boostLeft > 0.0f)
+        {
+            if (boostLeft < windupDuration)
+            {
+                float normalisedValue = this.boostLeft / this.windupDuration;
+                this.animator.speed = 1.0f + normalisedValue * (this.boostMultiplier - 1.0f);
+            }
+            else if (boostLeft > maxBoostDuration - windupDuration)
+            {
+                float normalisedValue = 1.0f - (this.boostLeft - this.maxBoostDuration + this.windupDuration) / this.windupDuration;
+                this.animator.speed = 1.0f + normalisedValue * (this.boostMultiplier - 1.0f);
+            }
+            else
+            {
+                this.animator.speed = 1.0f + (this.boostMultiplier - 1.0f);
+            }
+
+            boostLeft -= Time.deltaTime;
+        }
+        else
+        {
+            this.animator.speed = 1.0f;
+            this.boostLeft = 0;
+
+            this.isBoosting = false;
+        }
+	}
+
+    public void activateBoost()
+    {
+        if (this.isBoosting)
+        {
+            if (boostLeft < this.maxBoostDuration - this.windupDuration)
+            {
+                this.boostLeft = this.maxBoostDuration;
+                this.boostLeft -= boostLeft < this.windupDuration ? this.boostLeft : this.windupDuration;
+            }
+        }
+        else
+        {
+            this.boostLeft = this.maxBoostDuration;
+        }
+
+        this.isBoosting = true;
+    }
+}
